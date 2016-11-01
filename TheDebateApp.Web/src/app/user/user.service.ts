2a9363dccb704dbbacf8fa66ffcba48c138b47e2
Quinntyne Brown <quinntynebrown@gmail.com>
2016-11-01 18:07:29 -0400

    user

--- TheDebateApp.Web/src/app/user/user.service.ts
+++ TheDebateApp.Web/src/app/user/user.service.ts
@@ -2,6 +2,22 @@
 
 export class UserService {
 
+    public get() {
+        return fetch({ url: "/api/user/get" });
+    }
+
+    public getById(id) {
+        return fetch({ url: `/api/user/getbyid?id=${id}` });
+    }
+
+    public add(entity) {
+        return fetch({ url: `/api/user/add`, method: "POST", data: entity });
+    }
+
+    public remove(options: { id: number }) {
+        return fetch({ url: `/api/user/remove?id=${options.id}`, method: "DELETE" });
+    }
+
     public tryToLogin(options: { username: string, password: string }) {
         Object.assign(options, { grant_type: "password" });        
         return fetch({
@@ -10,6 +26,15 @@ export class UserService {
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             data: formEncode(options)
         });        
+    }
+
+    public regiser(options: { username: string, password: string }) {        
+        return fetch({
+            url: "/api/user/regiser",
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            data: formEncode(options)
+        });
     }
     
     public getCurrentUser = () => fetch({
